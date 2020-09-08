bbf5b399e4855d3c1f9290f0f8954d66eb41bc96
HenryTriplette <scrivi@triplette.it>
2020-09-08 18:20:51 +0200

    Added converter from epub to txt

--- generate-epub2txt.py
+++ generate-epub2txt.py
@@ -1,5 +1,12 @@
+#!/usr/bin/env python
+# Main script for converting *.epub into *.txt for processing
+import argparse
+import os
+import random
+import time
 import ebooklib
 
+from datetime import datetime
 from ebooklib import epub
 from bs4 import BeautifulSoup
 
@@ -43,3 +50,47 @@ def epub2text(epub_path):
     chapters = epub2thtml(epub_path)
     ttext = thtml2ttext(chapters)
     return ttext
+
+def content2txt(content, file):
+    filepath = file + ".txt"
+    f = open( filepath, 'a')
+    f.truncate()
+    # Compact content
+    f.write(','.join(content))
+    f.close()
+
+    return filepath
+
+def main(file_list):
+
+    # Process files
+    for file in file_list:
+        # Read
+        content = epub2text(file)
+        # Write
+        converted_file_path = content2txt(content, file)
+        # Log
+        print(converted_file_path)
+
+if __name__ == '__main__':
+
+    # Initialize
+    parser = argparse.ArgumentParser(description='Convert epub to txt.')
+    parser.add_argument('-f', '--folder', help='Source images folder', required=True)
+
+    args = parser.parse_args()
+
+    # Get the list of all files in directory tree at given path
+    listOfFiles = list()
+    for (dirpath, dirnames, filenames) in os.walk(args.folder):
+        listOfFiles += [os.path.join(dirpath, file) for file in filenames]
+
+    listOfBooks = list()
+    for elem in listOfFiles:
+        if elem.lower().endswith(('.epub')):
+            listOfBooks.append(elem)
+
+    try:
+        main(listOfBooks)
+    except KeyboardInterrupt:
+        print('Ended!')
